bcc59f89ae067c10116d7dbbf6bb69197e4fe935
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2023-07-15 00:59:41 -0500

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,9 +3,9 @@ import time
 
 def main():
     parser = argparse.ArgumentParser(description='Command-line utility example')
-    parser.add_argument('-u', '--username', nargs='?', const='', help='Username')
-    parser.add_argument('-p', '--password', nargs='?', const='', help='Password')
-    parser.add_argument('-ip', '--ip_address', nargs='?', const='', help='IP address')
+    parser.add_argument('-u', '--username', help='Username')
+    parser.add_argument('-p', '--password', help='Password')
+    parser.add_argument('-ip', '--ip_address', help='IP address')
 
     args = parser.parse_args()
 
